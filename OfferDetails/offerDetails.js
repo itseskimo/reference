57396260f3f92815e1c25e920f3c4499f450ca6d
ayush <ayushmatmotgill@gmail.com>
2022-12-26 03:03:30 -0800

    hh

--- OfferDetails/offerDetails.js
+++ OfferDetails/offerDetails.js
@@ -20,3 +20,51 @@ on('click', '.option', item => {
 	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
 	parent.innerText = item.target.innerText;
 })
+
+
+// --------------------------------------------------------------------
+const progress= document.getElementById('progress')
+const prev= document.getElementById('prev')
+const next= document.getElementById('next')
+const circles= document.querySelectorAll('.formCircle')
+let currentActive=0
+
+next.addEventListener('click',()=>{
+	currentActive++
+	if(currentActive>circles.length){
+		currentActive=circles.length
+	}
+    update()
+})
+
+prev.addEventListener('click',()=>{
+	currentActive--
+	if(currentActive < 0){
+		currentActive = 0
+	}
+	update()
+})
+
+function update(){
+	circles.forEach((circle,idx)=>{
+		
+		if(idx===currentActive){
+			 position='activeCircle'
+		}
+		if(idx<currentActive){
+			position='greenCircle'
+		}
+
+	
+    if(idx<currentActive){
+    	circle.classList.add(position)
+    }else if(idx===currentActive){
+		circle.classList.add(position)
+    }else{
+		circle.classList.remove(position)
+	}
+const actives=document.querySelectorAll('.greenCircle')
+console.log(actives.length,circles.length)
+
+	})
+}
