71ba7f589420babc6c10dd2920d12e1cfe835f6f
ayush <ayushmatmotgill@gmail.com>
2022-12-27 03:14:58 -0800

    hh

--- Requests/request.js
+++ Requests/request.js
@@ -22,13 +22,32 @@ window.addEventListener('load',()=>{
 		inputElement.classList.add('request');
 		inputElement.type = 'text';
 		inputElement.value = task;
+		inputElement.setAttribute('readonly', 'readonly');
 		inputCircle.appendChild(inputElement);
 
 		const img = document.createElement('img');
 		img.classList.add('editIcon');
 		img.src="https://cdn-icons-png.flaticon.com/512/61/61456.png"
-        section.appendChild(img)
+		img.alt='edit'
+        inputCircle.appendChild(img)
 
 		list.appendChild(section)
+
+        input.value=''
+        
+		img.addEventListener('click',()=>{
+			if(img.alt==='edit'){
+				img.src='https://www.pngfind.com/pngs/m/357-3570848_save-file-button-comments-save-file-icon-hd.png'
+				inputElement.removeAttribute("readonly");
+			    inputElement.focus();
+				img.alt='save'
+			}else{
+				img.src="https://cdn-icons-png.flaticon.com/512/61/61456.png"
+				img.alt='edit'
+				inputElement.setAttribute('readonly', 'readonly');
+			}
+			
+		})
+
 	 });
 })
