34f417251f8dc0c48a3d24abfe17b54049a082b2
ayush <ayushmatmotgill@gmail.com>
2022-12-27 22:40:10 -0800

    kk

---
+++ Submit/submit.js
@@ -0,0 +1,52 @@
+
+const prev= document.getElementById('prev')
+const next= document.getElementById('next')
+const circles= document.querySelectorAll('.formCircle')
+
+
+var value=parseInt(sessionStorage.getItem('value'))
+
+currentActive=value
+
+// let currentActive=0
+
+next.addEventListener('click',()=>{
+	++currentActive
+     window.location.href='/Lease/lease.html'
+	 sessionStorage.setItem('value',currentActive)
+    //  localStorage.clear()
+	 
+	if(currentActive>circles.length){
+		currentActive=circles.length
+	}
+    update()
+})
+
+prev.addEventListener('click',()=>{
+	window.location.href='/occupants/occupants.html'
+
+	currentActive--
+	if(currentActive < 0){
+		currentActive = 0
+	}
+	update()
+})
+
+function update(){
+	circles.forEach((circle,idx)=>{
+
+    if(idx<currentActive){
+    	circle.classList.add('greenCircle')
+		circle.classList.remove('activeCircle')
+    }else if(idx===currentActive){
+		circle.classList.add('activeCircle')
+		circle.classList.remove('greenCircle')
+    }else{
+		circle.classList.remove('activeCircle')
+	}
+// const actives=document.querySelectorAll('.greenCircle')
+// console.log(currentActive,actives.length,circles.length)
+	})
+}
+
+// --------------------------------------------------------------------
