f18d385f4c8d736169d2a60353fb647edc179204
ayush <ayushmatmotgill@gmail.com>
2022-12-28 10:20:15 -0800

    gg

---
+++ offerDetails.js
@@ -0,0 +1,69 @@
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
+
+
+// --------------------------------------------------------------------
+
+const prev= document.getElementById('prev')
+const next= document.getElementById('next')
+const circles= document.querySelectorAll('.formCircle')
+
+let currentActive=0
+
+next.addEventListener('click',()=>{
+	++currentActive
+    window.location.href='/Requests/request.html?value=1'
+	//sessionStorage.setItem('value',currentActive)
+
+	if(currentActive>circles.length){
+		currentActive=circles.length
+	}
+    update()
+})
+
+prev.addEventListener('click',()=>{
+	currentActive--
+	if(currentActive < 0){
+		currentActive = 0
+	}
+	update()
+})
+
+function update(){
+	circles.forEach((circle,idx)=>{
+
+    if(idx<currentActive){
+    	circle.classList.add('greenCircle')
+		circle.classList.remove('activeCircle')
+    }else if(idx===currentActive){
+		circle.classList.add('activeCircle')
+		circle.classList.remove('greenCircle')
+    }else{
+		circle.classList.remove('activeCircle')
+	}
+// const actives=document.querySelectorAll('.greenCircle')
+// console.log(currentActive,actives.length,circles.length)
+	})
+}
+
+// --------------------------------------------------------------------
