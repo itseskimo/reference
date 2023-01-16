a50a0f2aa43d69c1343a084ae75075689741b909
ayush <ayushmatmotgill@gmail.com>
2023-01-16 05:44:01 -0800

    r

--- offerDetails.js
+++ offerDetails.js
@@ -103,28 +103,28 @@ inputAmountSetter.value= `₹${inputAmountCalculator.value}`
 
 // --------------------------------------------------------------------
 
-const secondCalculator= document.querySelector('.secondDiv-Calculator')
-secondCalculator.style.display='none'
+// const secondCalculator= document.querySelector('.secondDiv-Calculator')
+// secondCalculator.style.display='none'
 
-// --------------------------------------------------------------------
+// // --------------------------------------------------------------------
 
-const hideBoxes= document.querySelectorAll('.option')
-const firstCalculator= document.getElementById('firstDiv-Calculator')
-let output;
-  hideBoxes.forEach((id)=>{
-	id.addEventListener('click',(e)=>{
-		if( firstCalculator.style.display ==='none'){
-			firstCalculator.style.display='block'
-			secondCalculator.style.display='none'
+// const hideBoxes= document.querySelectorAll('.option')
+// const firstCalculator= document.getElementById('firstDiv-Calculator')
+// let output;
+//   hideBoxes.forEach((id)=>{
+// 	id.addEventListener('click',(e)=>{
+// 		if( firstCalculator.style.display ==='none'){
+// 			firstCalculator.style.display='block'
+// 			secondCalculator.style.display='none'
 
-		}else{
-			firstCalculator.style.display='none'
-			secondCalculator.style.display='block'
-		}
-		output=`${e.currentTarget.innerText[0]} ${e.currentTarget.innerText[7]}`
-		yeartoMonths()
-	   })
-	})
+// 		}else{
+// 			firstCalculator.style.display='none'
+// 			secondCalculator.style.display='block'
+// 		}
+// 		output=`${e.currentTarget.innerText[0]} ${e.currentTarget.innerText[7]}`
+// 		yeartoMonths()
+// 	   })
+// 	})
 
 // --------------------------------------------------------------------
 
